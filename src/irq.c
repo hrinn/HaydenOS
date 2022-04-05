962d084b0451d4cac6e2ba8442410c564e0d0cf1
Hayden Rinn <hayden@haydenrinn.com>
2022-04-04 22:15:46 -0700

    Initial irq stuff

--- src/irq.c
+++ src/irq.c
@@ -1,5 +1,43 @@
 #include "irq.h"
+#include <stdint.h>
+
+// https://wiki.osdev.org/Interrupts_tutorial
+typedef struct {
+    uint16_t isr_low;       // Lower 16 bits of ISR's address
+    uint16_t kernel_cs;     // The GDT segement selector that the CPU will load into CS before calling the ISR
+    uint8_t ist;            // The IST in the TSS that teh CPU will load into RSP
+    uint8_t attributes;     // Type and attributes
+    uint16_t isr_mid;       // Middle 16 bits of ISR's address
+    uint32_t isr_high;      // Upper 32 bits of ISR's address
+    uint32_t reserved;      // 0
+} __attribute__((packed)) idt_entry_t;
+
+__attribute__((aligned(0x10)))
+static idt_entry_t idt[256];
 
 void IRQ_init() {
-    // Create an IDT
+    // Register IDT
+    // Configure PIC
+}
+
+// Mask determines which interrupts are enabled
+void IRQ_set_mask(int irq) {
+
+}
+
+void IRQ_clear_mask(int irq) {
+
+}
+
+int IRQ_get_mask(int IRQline) {
+
+}
+
+void IRQ_end_of_interrupt(int irq) {
+
+}
+
+void IRQ_set_handler(int irq, irq_handler_t handler, void *arg) {
+    idt_entry_t idt_entry = idt[irq];
+
 }
--- src/irq.h
+++ src/irq.h
@@ -8,6 +8,6 @@ extern int IRQ_get_mask(int IRQline);
 extern void IRQ_end_of_interrupt(int irq);
 
 typedef void (*irq_handler_t)(int, int, void *);
-extern void IRQ_set_handler(int irq, irq_handler_t handler, void *arg);
+extern void IRQ_set_handler(uint8_t irq, irq_handler_t handler, void *arg);
 
 #endif
